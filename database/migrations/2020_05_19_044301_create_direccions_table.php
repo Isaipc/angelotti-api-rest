a6e3bf6558effde6f19509c3cb86e682a6264d1c
Isai Polito <ispolitoc@gmail.com>
2020-05-19 00:25:55 -0500

    init

---
+++ database/migrations/2020_05_19_044301_create_direccions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDireccionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('direccions', function (Blueprint $table) {
+            $table->id();
+            $table->string('calle');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('num_ext');
+            $table->string('num_int');
+            $table->string('referencia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('direccions');
+    }
+}
