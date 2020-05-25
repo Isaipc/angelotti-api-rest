b97134f764d7a012430727f47d2cf93b6c7eef50
Isai Polito <ispolitoc@gmail.com>
2020-05-24 22:11:41 -0500

    modificar migracion platillos

--- database/migrations/2020_05_19_044246_create_platillos_table.php
+++ database/migrations/2020_05_19_044246_create_platillos_table.php
@@ -15,9 +15,9 @@ class CreatePlatillosTable extends Migration
     {
         Schema::create('platillos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre', 60);
+            $table->string('nombre');
             $table->double('precio');
-            $table->string('imagen');
+            $table->string('imagen', 2000);
             $table->unsignedBigInteger('categoria_id');
             $table->foreign('categoria_id')->references('id')->on('categorias');
             $table->timestamps();
